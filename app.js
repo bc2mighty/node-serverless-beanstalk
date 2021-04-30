314e7c9321678953ff05995beb1d441cd5007e6d
bc2mighty <akinadetunmise8@gmail.com>
2021-04-30 16:49:22 +0100

    Serverless Setup for Lambda

--- app.js
+++ app.js
@@ -72,8 +72,4 @@ app.post("/register", async (req, res) => {
   }
 })
 
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-	console.log(`listening on port ${port}`);
-});
+module.exports = app
